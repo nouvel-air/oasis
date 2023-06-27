70745c465d3591d76bbfbedbbe263a2c487ec079
srosset81 <srosset81@gmail.com>
2023-06-27 12:46:16 +0200

    Refactoring

--- dest-oasis/src/pages/Hero.jsx
+++ dest-oasis/src/pages/Hero.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Container, Box, Grid, Typography, Button, Select, MenuItem, Input } from '@mui/material';
+import { Container, Box, Grid, Typography } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faCircleArrowRight, faCirclePlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faCircleArrowRight, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import backgroundImage from '../assets/background.jpg';
 import { ReactComponent as Title } from '../assets/Titre_soustitre_home.svg';
+import TopLink from './TopLink';
 import Filters from './Filters';
-
-const TopLink = ({ icon, children, align = "left", iconColor = 'white' }) => {
-  const iconStyle = { marginRight: 10, position: 'relative', top: 5, height: 26 };
-  if (align === 'left') iconStyle.marginRight = 10;
-  if (align === 'right') iconStyle.marginLeft = 10;
-  const faIcon = <FontAwesomeIcon icon={icon} color={iconColor} style={iconStyle} />;
-  return (
-    <Typography color="white" variant="body2" align={align} mb={1}>
-      {align === "left" && faIcon}
-      {children}
-      {align === "right" && faIcon}
-    </Typography>
-  )
-}
+import SearchForm from './SearchForm';
 
 const Hero = () => {
   const theme = useTheme();
@@ -61,23 +48,7 @@ const Hero = () => {
               </Grid> 
             </Grid>
           </Box>
-          <Box display="flex" mt={3} mb={3} sx={{ width: 600 }} alignItems="center" justifyContent="center">
-            <Input 
-              disableUnderline
-              placeholder="Mots-clés" 
-              sx={{ margin: 1, paddingLeft: 3, paddingTop: '3px', height: 40, backgroundColor: 'white', borderRadius: 10 }} 
-              fullWidth 
-            />
-            <Select variant="standard" disableUnderline sx={{ height: 40, margin: 1, paddingLeft: 3, paddingTop: '3px', backgroundColor: 'white', borderRadius: 10, '& .MuiSelect-icon': { right: 10 } }} fullWidth>
-              {/* <MenuItem disabled value=""><em>Région</em></MenuItem> */}
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-            <Button sx={{ backgroundColor: 'primary.main', borderRadius: '100%', minWidth: 40, width: 40, height: 40, margin: 1 }}>
-              <FontAwesomeIcon icon={faMagnifyingGlass} color="white" fontSize={18} />
-            </Button>
-          </Box>
+          <SearchForm />
           <Typography color="white" sx={{ fontWeight: 500, fontSize: 24, marginTop: 3, marginBottom: 4 }}>
             Types d'hébergements
           </Typography>
