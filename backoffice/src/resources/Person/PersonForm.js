301561d11586a240128de07bf2a151338016d1b7
srosset81 <srosset81@gmail.com>
2023-07-13 18:13:27 +0200

    Do not allow user to change his places

--- backoffice/src/resources/Person/PersonForm.js
+++ backoffice/src/resources/Person/PersonForm.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { SimpleForm, TextInput } from 'react-admin';
 import { PlacesInput } from '../../common/input';
+import useIsAdmin from '../../hooks/useIsAdmin';
 
-export const PersonForm = () => (
-  <SimpleForm>
-    <TextInput source="pair:firstName" fullWidth />
-    <TextInput source="pair:lastName" fullWidth />
-    <PlacesInput source="cdlt:proposes" fullWidth />
-  </SimpleForm>
-);
+export const PersonForm = () => {
+  const isAdmin = useIsAdmin();
+  return (
+    <SimpleForm>
+      <TextInput source="pair:firstName" fullWidth />
+      <TextInput source="pair:lastName" fullWidth />
+      {isAdmin && <PlacesInput source="cdlt:proposes" fullWidth />}
+    </SimpleForm>
+  );
+};
 
 export default PersonForm;
