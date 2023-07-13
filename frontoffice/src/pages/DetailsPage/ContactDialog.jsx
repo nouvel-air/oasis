d93da451278350b3edfdbaeee509c38e1ce59107
srosset81 <srosset81@gmail.com>
2023-07-13 19:44:28 +0200

    Working contact dialog

--- frontoffice/src/pages/DetailsPage/ContactDialog.jsx
+++ frontoffice/src/pages/DetailsPage/ContactDialog.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Form, TextInput } from 'react-admin';
+import { Form, TextInput, useRecordContext, useNotify } from 'react-admin';
 import { Button, Dialog, DialogContent, DialogContentText, DialogActions, Typography, useMediaQuery } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 
 const ContactDialog = ({ open, onClose, service }) => {
   const xs = useMediaQuery(theme => theme.breakpoints.down('sm'), { noSsr: true });
-  const onSubmit = ({ name, email, message }) => {
-    console.log('values', name, email, message);
+  const record = useRecordContext();
+  const notify = useNotify();
+
+  const onSubmit = async ({ name, email, content }) => {
+    const result = await fetch(process.env.REACT_APP_MIDDLEWARE_URL + '_mailer/contact', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ resourceUri: record['@id'] || record.id, name, email, content })
+    });
+  
+    if (result.ok) {
+      onClose();
+      notify('Votre message a bien été envoyé', { type: 'success' });
+    } else {
+      const json = await result.json();
+      notify(json ? json.message : "Votre message n'a pas pu être envoyé", { type: 'error' });
+    }
   }
+
   return (
     <Dialog open={open} onClose={onClose}>
-      <Form onSubmit={onSubmit} defaultValues={{ message: service ? `Je suis intéressé par le service "${service['pair:label']}".` : "" }}>
+      <Form onSubmit={onSubmit} defaultValues={{ content: service ? `Je suis intéressé par le service "${service['pair:label']}".` : "" }}>
         <DialogContent sx={{ pt: 3, pb: 0, width: xs ? undefined : 500 }}>
           <Typography variant="h4" sx={{ mb: 2 }}>
             Formulaire de contact
@@ -18,7 +34,7 @@ const ContactDialog = ({ open, onClose, service }) => {
           <DialogContentText>
             <TextInput source="name" label="Votre nom" fullWidth />
             <TextInput source="email" label="Adresse mail" fullWidth />
-            <TextInput source="message" label="Votre message" fullWidth multiline rows={5} />
+            <TextInput source="content" label="Votre message" fullWidth multiline rows={5} />
           </DialogContentText>
         </DialogContent>
         <DialogActions>
