4dc7abc33f2d30f0c99370f7a381105213b8466c
srosset81 <srosset81@gmail.com>
2023-07-20 15:58:17 +0200

    Fix BackgroundSlider

--- frontoffice/src/pages/HomePage/BackgroundSlider.jsx
+++ frontoffice/src/pages/HomePage/BackgroundSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import backgroundImage1 from '../../assets/background1.jpg';
@@ -6,26 +6,53 @@ import backgroundImage2 from '../../assets/background2.jpg';
 import backgroundImage3 from '../../assets/background3.jpg';
 
 const BackgroundSlider = ({ children, ...rest }) => {
-  const [visible, setVisible] = useState(3)
-  setInterval(() => {
-    if (visible === 3) {
-      setVisible(1);
-    } else {
-      setVisible(visible+1);
-    }
-  }, 7000);
+  const [visible, setVisible] = useState(3);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (visible === 3) {
+        setVisible(1);
+      } else {
+        setVisible(visible+1);
+      }
+    }, 7000);
+    return () => clearInterval(intervalId); 
+  }, [visible, setVisible]);
+
   return (
     <Box position="relative" minHeight={500} {...rest}>
-      <BackgroundImage src={backgroundImage1} opacity={visible === 1 ? 1 : 0} />
-      <BackgroundImage src={backgroundImage2} opacity={visible === 2 ? 1 : 0} />
-      <BackgroundImage src={backgroundImage3} opacity={visible === 3 ? 1 : 0} />
+      <BackgroundImage src={backgroundImage1} opacity={visible === 1 ? 1 : 0} delay="0s" />
+      <BackgroundImage src={backgroundImage2} opacity={visible === 2 ? 1 : 0} delay="7s" />
+      <BackgroundImage src={backgroundImage3} opacity={visible === 3 ? 1 : 0} delay="14s" />
       <BlackFilter />
       {children}
     </Box>
   )
 };
 
-const BackgroundImage = styled('div')(({ src, opacity }) => ({
+// https://tympanus.net/codrops/2012/01/02/fullscreen-background-image-slideshow-with-css3/
+const BackgroundImage = styled('div')(({ src, opacity, delay }) => ({
+  // "@keyframes imageAnimation": {
+  //   "0%": {
+  //     opacity: 0,
+  //     animationTimingFunction: "ease-in"
+  //   },
+  //   "16%": {
+  //     opacity: 1,
+  //     animationTimingFunction: "ease-out"
+  //   },
+  //   "33%": {
+  //     opacity: 1
+  //   },
+  //   "50%": {
+  //     opacity: 0
+  //   },
+  //   "100%": {
+  //     opacity: 0
+  //   },
+  // },
+  // animation: "imageAnimation 21s linear infinite 0s",
+  // animationDelay: delay,
   backgroundImage: `url(${src})`,
   backgroundPosition: "center",
   backgroundSize: "cover",
