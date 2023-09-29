48d2944d62327bade453c9c564a153a8b5ebdc90
srosset81 <srosset81@gmail.com>
2023-09-29 17:16:11 +0200

    Remove matomo tags tracking

--- frontoffice/src/App.js
+++ frontoffice/src/App.js
@@ -14,42 +14,32 @@ import CustomPage from './pages/CustomPage/CustomPage';
 
 const LoginPage = () => <LocalLoginPage hasSignup={false} />
 
-const App = () => {
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      let _mtm = window._mtm = window._mtm || [];
-      _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-      let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src='https://stats.colibris-outilslibres.org/js/container_vjn8ur2M.js'; s.parentNode.insertBefore(g,s);
-    } 
-  }, []);
-  return (
-    <BrowserRouter>
-      <Admin
-        disableTelemetry
-        title="Destination Oasis"
-        authProvider={authProvider}
-        dataProvider={dataProvider}
-        i18nProvider={i18nProvider}
-        theme={theme}
-        loginPage={LoginPage}
-        store={memoryStore()}
-      >
-        <CustomRoutes noLayout>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/lieux/:slug" element={<DetailsPage />} />
-          <Route path="/pages/:slug" element={<CustomPage />} />
-        </CustomRoutes>
-        <Resource name="Page" />
-        <Resource name="Person" />
-        <Resource name="Place" />
-        <Resource name="Region" />
-        <Resource name="Service" />
-        <Resource name="Status" />
-        <Resource name="Type" />
-      </Admin>
-    </BrowserRouter>
-  )
-};
+const App = () => (
+  <BrowserRouter>
+    <Admin
+      disableTelemetry
+      title="Destination Oasis"
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      i18nProvider={i18nProvider}
+      theme={theme}
+      loginPage={LoginPage}
+      store={memoryStore()}
+    >
+      <CustomRoutes noLayout>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/lieux/:slug" element={<DetailsPage />} />
+        <Route path="/pages/:slug" element={<CustomPage />} />
+      </CustomRoutes>
+      <Resource name="Page" />
+      <Resource name="Person" />
+      <Resource name="Place" />
+      <Resource name="Region" />
+      <Resource name="Service" />
+      <Resource name="Status" />
+      <Resource name="Type" />
+    </Admin>
+  </BrowserRouter>
+);
 
 export default App;
